b8a0b7535f810448fd8208160bd6da8948072401
CaoChensy <867529463@qq.com>
2025-02-21 17:07:45 +0800

    20240222: test local tools call

--- test/agent/test_agent.py
+++ test/agent/test_agent.py
@@ -28,4 +28,31 @@ class TestToolsAgent(unittest.TestCase):
             # print(chunk)
             if chunk.choices[0].delta.content:
                 answer += chunk.choices[0].delta.content
-#         print(answer)
+        print(answer)
+
+
+class TestLocalToolsAgent(unittest.TestCase):
+    """"""
+    def setUp(self):
+        """"""
+        self.base_url = "http://172.16.11.159:8000/"
+        self.model = "Qwen2.5-14B-Instruct"
+        self.llm = OpenAI(
+            model=self.model, base_url=self.base_url,
+            api_key="empty", verbose=False)
+        self.tools = Tools(tools=[random_number])
+
+    def test_tools_agent(self):
+        """"""
+        agent = ToolsAgent(tools=self.tools, llm=self.llm, verbose=True)
+        agent.completion(prompt="你好，请给我一个0-100的随机数")
+
+    def test_tools_agent_sse(self):
+        """"""
+        agent = ToolsAgent(tools=self.tools, llm=self.llm, verbose=True)
+        completion = agent.completion(prompt="你好，请给我一个0-100的随机数", stream=True)
+        answer = ""
+        for chunk in completion:
+            if chunk.choices[0].delta.content:
+                answer += chunk.choices[0].delta.content
+        print(answer)
